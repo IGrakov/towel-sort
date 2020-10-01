153260009c7f0ee46c64c5a6a0309777c390e27b
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-01 15:46:27 +0300

    feat: add towel sort algorithm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+    let resArr = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 === 0) {
+            matrix[i].sort(a, b => a - b);
+        } else {
+            matrix[i].sort(a, b => b - a);
+        }
+        resArr.concat(matrix[i]);
+    }
+
+    return resArr;
 }
