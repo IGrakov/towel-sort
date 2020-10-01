4e6f1491b0cef35d3f9a3eb2ef95902179c80034
Ivan Grakov <ivan.grakov@gmail.com>
2020-10-01 15:48:21 +0300

    refactor: add comments

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ module.exports = function towelSort (matrix) {
 
     for (let i = 0; i < matrix.length; i++) {
         if (i % 2 === 0) {
+            // if raw is even sorting forward
             matrix[i].sort(a, b => a - b);
         } else {
+            // if raw is odd sorting backward
             matrix[i].sort(a, b => b - a);
         }
+        // adding raw to resulting matrix
         resArr.concat(matrix[i]);
     }
 
